8e26772625efa80b776a277e5cfd88862ba7a23a
Takashi Oshikawa <abr3tks5200@ezweb.ne.jp>
2015-11-08 09:25:41 +0900

    change front base port

--- main/top_assets/js/env.js
+++ main/top_assets/js/env.js
@@ -2,11 +2,12 @@
 
 var current_location = window.location.hostname;
 
-var BASE_URL       = current_location == 'shymotion.tokyo' ? 'shymotion.tokyo' : '127.0.0.1';
-var SCHEME         = 'http://';
-var BACK_BASE_PORT = current_location == 'shymotion.tokyo' ? ':65000/' : ':8080/';
-var BACK_BASE_URL  = SCHEME + BASE_URL + BACK_BASE_PORT;
-var FRONT_BASE_URL = SCHEME + BASE_URL + ':4000/';
+var BASE_URL        = current_location == 'shymotion.tokyo' ? 'shymotion.tokyo' : '127.0.0.1';
+var SCHEME          = 'http://';
+var BACK_BASE_PORT  = current_location == 'shymotion.tokyo' ? ':65000/' : ':8080/';
+var FRONT_BASE_PORT = current_location == 'shymotion.tokyo' ? '/' : ':4000/';
+var BACK_BASE_URL   = SCHEME + BASE_URL + BACK_BASE_PORT;
+var FRONT_BASE_URL  = SCHEME + BASE_URL + FRONT_BASE_PORT;
 
 var url = new Vue({
 	el: '.url',
